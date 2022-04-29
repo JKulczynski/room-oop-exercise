b785b66f78880401fc3d081c3bb4a4a95aaace91
JKulczynski <j.kulczynski@adform.com>
2022-04-29 18:09:25 +0200

    Final Commit

--- src/main/scala/Room.scala
+++ src/main/scala/Room.scala
@@ -1,4 +1,20 @@
-class Room(roomName: Int, capacity: Int) {
+class Room(roomName: String, capacity: Int) {
 
-  capacity
+  var occupied = 0
+
+  def enter(newPeople: Int): Boolean = {
+    if (newPeople + occupied > capacity) return false
+    else occupied += newPeople
+    true
+  }
+
+  def leave(leavingPeople: Int): Boolean = {
+    if (occupied - leavingPeople < 0) false
+    else {
+      occupied -= leavingPeople
+      true
+    }
+  }
+
+  val report = println(s"in $roomName is occupied by $occupied people")
 }
--- src/test/scala/RoomSpec.scala
+++ src/test/scala/RoomSpec.scala
@@ -6,12 +6,17 @@ class RoomSpec extends AnyFlatSpec with should.Matchers{
   behavior of "Room"
 
   it should "allow people to enter" in {
-
+    val room = new Room("Kitchen", 10)
+    val actual = room.enter(10)
+    val expected = true
+    actual shouldBe expected
   }
 
   it should "allow people to leave" in {
-
+    val room = new Room("LivingRoom", 15)
+    val actual = room.leave(16)
+    val expected = false
+    actual shouldBe expected
   }
 
-  it should "report number of people inside"
 }
